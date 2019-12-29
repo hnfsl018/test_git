7f056dc96df429b85ba8f9bddd3bec197b746ebf
Ridwan <Ridwan72th@gmail.com>
2019-12-29 23:32:41 +0700

    pangporn react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    input: "",
+    result: ""
+    
+  };
+
+  
+  testChange = event => {
+    const value = event.target.value;
+    this.setState({
+      input: value
+    });
+  };
+
+  receiveValue = event => {
+    event.preventDefault();
+    const text = this.state.input;
+    this.setState({
+      result: text
+    });
+  };
+
+  delete = event => {
+    const del = this.state.result;
+    this.setState({
+      result: null
+    });
+  };
+
+  edit = () => {
+    
+    this.setState({
+      input: this.state.result
+    });
+  };
+  
+
+  render() {
+    return (
+      <React.Fragment>
+        <form onSubmit={this.receiveValue}>
+          <div>
+            <input type="text" value={this.state.input} onChange={this.testChange}/>
+          </div>
+          <div>
+            <button>ADD</button>
+            
+          </div>
+        </form>
+        <button onClick={this.delete}>delete</button>
+        <button onClick={this.edit}>edit</button>
+        <p>Text: {this.state.result}</p>
+      
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
